912ad5ed766c9b90a19c05e18b0655ea7e64c2bc
Luis Matos <luis.felipe.pereira.matos@gmail.com>
2022-12-30 18:35:14 -0300

    finalizando crud despesas

--- controller/despesaController.go
+++ controller/despesaController.go
@@ -41,6 +41,83 @@ func CadastrarDespesa(c *gin.Context) {
 
 }
 
+func AtualizarDespesaPorID(c *gin.Context) {
+
+	id := c.Params.ByName("id")
+
+	despesa := service.BuscarDespesaId(id)
+
+	if despesa.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"mensagem": "Despesa não encontrada",
+			"status":   404})
+		return
+	}
+
+	var despesaDTO model.DespesaDTO
+
+	log.Print("Iniciando atualização da despesa!")
+
+	if err := c.ShouldBindJSON(&despesaDTO); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+
+	log.Print("Convertendo para json")
+
+	if err := model.ValidaDadosDespesa(&despesaDTO); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"erro": err.Error()})
+		return
+	}
+
+	log.Print("Campos validados com sucesso! ", despesaDTO)
+
+	despesaAtualizada, jaCadastrado := service.AtualizarDespesa(&despesaDTO, id)
+
+	if jaCadastrado {
+		log.Println("Despesa cadastrada anteriormente!")
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"message": "Despesa já cadastrada nesse mês!", "status": 422})
+	} else {
+		c.JSON(http.StatusOK, despesaAtualizada)
+	}
+}
+
+func DeletarDespesaPorID(c *gin.Context) {
+
+	id := c.Params.ByName("id")
+
+	receita := service.BuscarDespesaId(id)
+
+	if receita.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"mensagem": "Despesa não encontrado",
+			"status":   404})
+		return
+	}
+
+	service.DeletarDespesaPorID(id)
+
+	c.JSON(http.StatusNoContent, nil)
+}
+
+func BuscarDespesaId(c *gin.Context) {
+
+	id := c.Params.ByName("id")
+
+	despesa := service.BuscarDespesaId(id)
+
+	if despesa.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"mensagem": "Despesa não encontrada!",
+			"status":   404})
+		return
+	}
+
+	c.JSON(http.StatusOK, despesa)
+}
+
 func BuscarTodasDespesas(c *gin.Context) {
 
 	log.Println("Iniciando busca de todas as despesas!")
